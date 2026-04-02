599cd5b196aa14cbf2dc6ebc3cc2ee6dd40e90d5
Ikram-code-ai <ikram.benchalal@etu.unice.fr>
2026-04-03 01:45:32 +0200

    gerer ci : ajout pygame/requests au pipeline, import renderer optionnel

--- game/__init__.py
+++ game/__init__.py
@@ -4,4 +4,8 @@ from game.environment import Environment
 from game.agents import HumanAgent, BlinkyGhost, PinkyGhost, InkyGhost, ClydeGhost, GhostMode
 from game.engine import GameEngine
 from game.recorder import GameRecorder
-from game.renderer import PacmanRenderer
+
+try:
+    from game.renderer import PacmanRenderer
+except ImportError:
+    PacmanRenderer = None
